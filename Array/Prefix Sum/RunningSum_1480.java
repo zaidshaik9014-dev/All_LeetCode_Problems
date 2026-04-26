eb7890f93d08956226cbb0dffbb221f7b519cfed
zaidshaik9014-dev <zaidshaik9014@gmail.com>
2026-04-26 14:20:20 +0530

    Added deleted files

---
+++ Array/Prefix Sum/RunningSum_1480.java
@@ -0,0 +1,19 @@
+public class RunningSum_1480 {
+
+    public static int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+
+        int[] result = runningSum(nums);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+}
