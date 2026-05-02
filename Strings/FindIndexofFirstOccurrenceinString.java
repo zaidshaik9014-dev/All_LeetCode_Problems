014bb73b7342d0c97e9c81e708b3d2015944666f
zaidshaik9014-dev <zaidshaik9014@gmail.com>
2026-05-02 10:24:04 +0530

    Tested for errors

--- Strings/FindIndexofFirstOccurrenceinString.java
+++ Strings/FindIndexofFirstOccurrenceinString.java
@@ -27,3 +27,14 @@ public class FindIndexofFirstOccurrenceinString { //28
         System.out.println(strStr(haystack, needle));
     }
 }
+// Example 1:
+
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+// Example 2:
+
+// Input: haystack = "leetcode", needle = "leeto"
+// Output: -1
+// Explanation: "leeto" did not occur in "leetcode", so we return -1.
