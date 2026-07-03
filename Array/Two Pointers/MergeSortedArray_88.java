0a3461060ec1b725b6a7eaeb6ef9441f7994385a
zaidshaik9014-dev <zaidshaik9014@gmail.com>
2026-07-03 10:46:43 +0530

    Newly SOlved

---
+++ Array/Two Pointers/MergeSortedArray_88.java
@@ -0,0 +1,36 @@
+public class MergeSortedArray_88 {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        while (j >= 0) {
+            nums1[k] = nums2[k];
+            j--;
+            k--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+        merge(nums1, m, nums2, n);
+        System.out.print("Merged Array: ");
+        for (int num : nums1) {
+            System.out.print(num + " ");
+        }
+    }
+}
